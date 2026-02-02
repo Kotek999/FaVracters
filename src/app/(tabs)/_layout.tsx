15ae16ca60ca5ac969491f1ecaf85aa7179e8409
Filip <filiptankiewicz04@gmail.com>
2026-02-03 00:52:38 +0100

    ui library added

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,38 +1,47 @@
+import * as eva from "@eva-design/eva";
 import { Tabs } from "expo-router";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ApplicationProvider } from "@ui-kitten/components";
 
 export default function TabsLayout() {
   return (
-    <SafeAreaProvider>
-      <Tabs screenOptions={{ headerShown: false }}>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "Home",
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <SafeAreaProvider>
+        <Tabs
+          screenOptions={{
+            headerShown: false,
+            tabBarStyle: { backgroundColor: "#102222", borderColor: "#1da5a5" },
           }}
-        />
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Home",
+            }}
+          />
 
-        <Tabs.Screen
-          name="second"
-          options={{
-            title: "Second",
-          }}
-        />
+          <Tabs.Screen
+            name="second"
+            options={{
+              title: "Second",
+            }}
+          />
 
-        <Tabs.Screen
-          name="third"
-          options={{
-            title: "Third",
-          }}
-        />
+          <Tabs.Screen
+            name="third"
+            options={{
+              title: "Third",
+            }}
+          />
 
-        <Tabs.Screen
-          name="fourth"
-          options={{
-            title: "Fourth",
-          }}
-        />
-      </Tabs>
-    </SafeAreaProvider>
+          <Tabs.Screen
+            name="fourth"
+            options={{
+              title: "Fourth",
+            }}
+          />
+        </Tabs>
+      </SafeAreaProvider>
+    </ApplicationProvider>
   );
 }
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,32 @@
-import { View, Text, Button } from "react-native";
+import { Layout, Text, Button } from "@ui-kitten/components";
 import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Second Screen"
-        onPress={() => router.push("/second")}
-      />
-    </View>
+    <Layout
+      style={{
+        flex: 1,
+        backgroundColor: "#132525",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <SafeAreaView>
+        <Text category="h3" style={{ color: "white" }}>
+          HOME SCREEN
+        </Text>
+        <Button
+          appearance="ghost"
+          status="info"
+          style={{
+            backgroundColor: "#0df2f2",
+          }}
+          onPress={() => router.push("/second")}
+        >
+          Go to Second Screen
+        </Button>
+      </SafeAreaView>
+    </Layout>
   );
 }
